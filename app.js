a39211e21bd959bf9bdce207cd25f100389b49f5
sorani-dev <sorani.dev@outlook.com>
2021-11-29 13:09:12 +0100

    feat: add pug view engine

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express')
+const path = require('path')
 
 // Init app
 const app = express()
 
+// Load View Engine
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','pug')
 
 // Routes
+// Home Route
 app.get('/',function (req,res) {
-    res.send('hello');
+    res.render('index',{
+        title: 'Articles'
+    });
 })
 
 const PORT = process.env.PORT || 5000
