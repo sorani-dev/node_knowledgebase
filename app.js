154df86dbe909531c0af1c35654f97bbc20a003f
sorani-dev <sorani.dev@outlook.com>
2021-11-29 13:10:02 +0100

    feat: add the add article route

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ app.get('/',function (req,res) {
     });
 })
 
+// Add Article Route
+app.get('/articles/add',function (req,res) {
+    res.render('add_article',{
+        title: 'Add Article'
+    })
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT,() => console.log(`Server started on port ${PORT}`))
