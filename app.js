a1cfe5064ea9950e3d99ea0b6c98bd15033dc198
sorani-dev <sorani.dev@outlook.com>
2021-11-29 14:14:38 +0100

    feat: add article to MongoDB by POST route

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const path = require('path')
 
+const bodyParser = require('body-parser')
 const express = require('express')
 const mongoose = require('mongoose')
 
@@ -24,6 +25,13 @@ const app = express()
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','pug')
 
+// Body Parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 // Bring in the Models
 const Article = require('./models/article');
 
@@ -47,6 +55,21 @@ app.get('/',function (req,res) {
 app.get('/articles/add',function (req,res) {
     res.render('add_article',{
         title: 'Add Article'
+    })
+})
+
+// Add Article Submit POST Route
+app.post('/articles/add',function (req,res) {
+    const article = new Article()
+    article.title = req.body.title
+    article.author = req.body.author
+    article.body = req.body.body
+    article.save(function (err) {
+        if (err) {
+            console.error(err)
+            return
+        }
+        res.redirect('/')
     })
 })
 
