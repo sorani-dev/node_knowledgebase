8e372e9887eea37629df4754e67bbefad3490b97
sorani-dev <sorani.dev@outlook.com>
2021-11-30 00:28:42 +0100

    feat: add DOM alert, check for fetch result - add alert in the DOM from the fetch request - check for success in deleting the article

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,40 @@
 document.querySelectorAll('.delete-article').forEach(article => {
     article.addEventListener('click',e => {
         if (confirm('Delete article?') === true) {
+            // Get the article id
             const id = e.target.getAttribute('data-id')
             const request = async () => {
                 try {
-                    const response = await fetch(`/article/${id}`,{
+                    const response = await fetch(`/articles/${id}`,{
                         method: 'DELETE'
                     });
                     const data = await response.json();
                     console.log(response,data);
-                    if (response.ok) {
-                        // console.log(data);
-                        alert('Deleted article')
-                        window.location.href = '/'
+                    // Response is ok and the article was deleted
+                    if (response.ok && data.success === true) {
+                        console.log(data);
+                        insert(`<div class="alert alert-success">Deleted article<br>Wait while you are redirected...</div>`)
+                        // alert('Deleted article')
+                        setTimeout(() => {
+                            window.location.href = '/'
+                        },1000);
+                        return false
                     }
                     throw data
                 } catch (err) {
                     console.error(err);
-                    alert('Could not delete article')
+                    // alert('Could not delete article')
+                    insert(`<div class="alert alert-danger">Could not delete article</div>`)
                 }
             }
             request()
         }
         e.preventDefault()
     })
+
+    function insert(text) {
+        const d = document.createElement('div')
+        d.innerHTML = text
+        document.querySelector('.container').insertBefore(d,document.querySelector('h1'))
+    }
 })
