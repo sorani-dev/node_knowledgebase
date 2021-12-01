1bf2a1650ca1ddae6af293d3778d5d3a0613dc4a
sorani-dev <sorani.dev@outlook.com>
2021-12-01 21:41:49 +0100

    feat: add Xhr header and remove alert messages already there

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,17 @@
 document.querySelectorAll('.delete-article').forEach(article => {
     article.addEventListener('click',e => {
+        e.preventDefault()
+
         if (confirm('Delete article?') === true) {
             // Get the article id
             const id = e.target.getAttribute('data-id')
             const request = async () => {
                 try {
                     const response = await fetch(`/articles/${id}`,{
-                        method: 'DELETE'
+                        method: 'DELETE',
+                        headers: {
+                            'X-Requested-With': 'XMLHttpRequest'
+                        }
                     });
                     const data = await response.json();
                     console.log(response,data);
@@ -24,17 +29,21 @@ document.querySelectorAll('.delete-article').forEach(article => {
                 } catch (err) {
                     console.error(err);
                     // alert('Could not delete article')
-                    insert(`<div class="alert alert-danger">Could not delete article</div>`)
+                    insert(`<div class="alert alert-danger">Could not delete article</div>`,true)
                 }
             }
             request()
         }
-        e.preventDefault()
     })
 
-    function insert(text) {
+    function insert(text,removePrevious = true) {
         const d = document.createElement('div')
         d.innerHTML = text
+        if (removePrevious) {
+            document.querySelectorAll('.alert').forEach(alert => {
+                alert.parentElement.removeChild(alert)
+            })
+        }
         document.querySelector('.container').insertBefore(d,document.querySelector('h1'))
     }
 })
